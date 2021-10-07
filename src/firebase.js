83d15a019abe9ff4f4564287c554b6eb317951a8
Vignesh Ganesan <58505455+Vignesh-Ganesan-008@users.noreply.github.com>
2021-10-07 21:20:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import "firebase/storage";
 import "firebase/database";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCaeDwSPJnIQ0lgD2HOvZbklZDgndYpBQ8",
+	apiKey: "AIzaSyCaeDwSPJnIQ0lgD2HOvZbklZDgndYpBQ8", //api jey for firestore
 	authDomain: "geodump-27582.firebaseapp.com",
-	projectId: "geodump-27582",
-	storageBucket: "geodump-27582.appspot.com",
+	projectId: "geodump-27582", // project name
+	storageBucket: "geodump-27582.appspot.com", // buckey id
 	messagingSenderId: "751887791497",
 	appId: "1:751887791497:web:7e8c46a8dee5235eec5d58",
 };
